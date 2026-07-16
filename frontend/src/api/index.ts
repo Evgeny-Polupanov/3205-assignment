8093135e00bec7458d328fa402f12fc7102332ba
Evgeniy Polupanov <epolupanov@catapulto.ru>
2026-07-16 18:38:43 +0300

    feat: set up rtk-query api

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface JobUrl {
+  url: string;
+  status: 'pending' | 'in_progress' | 'success' | 'error' | 'cancelled';
+  httpStatus: number | null;
+  errorMessage: string | null;
+  startTime: string | null;
+  endTime: string | null;
+  duration: number | null;
+}
+
+export interface JobSummary {
+  id: string;
+  createdAt: string;
+  status: 'pending' | 'in_progress' | 'completed' | 'failed' | 'cancelled';
+  urlsCount: number;
+  urlsStats: [number, number];
+}
+
+export interface Job extends JobSummary {
+  urls: JobUrl[];
+}
+
+export const jobsApi = createApi({
+  reducerPath: 'jobsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: '/api/jobs' }),
+  tagTypes: ['Jobs'],
+  endpoints: (builder) => ({
+    getJobs: builder.query<JobSummary[], void>({
+      query: () => '',
+      providesTags: (result) => result
+        ? [
+          ...result.map(({ id }) => ({
+            type: 'Jobs' as const,
+            id,
+          })),
+          { type: 'Jobs', id: 'LIST' },
+        ]
+        : [{ type: 'Jobs', id: 'LIST' }],
+    }),
+    getJobUrls: builder.query<JobUrl[], string>({
+      query: (id) => `/${id}`,
+      providesTags: (_result, _error, id) => [{ type: 'Jobs', id }],
+    }),
+    createJob: builder.mutation<{ jobId: string }, { urls: string[] }>({
+      query: (body) => ({
+        url: '',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: [{ type: 'Jobs', id: 'LIST' }],
+    }),
+    cancelJob: builder.mutation<Job, string>({
+      query: (id) => ({
+        url: `/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (_result, _error, id) => [
+        { type: 'Jobs', id },
+        { type: 'Jobs', id: 'LIST' },
+      ],
+    }),
+  }),
+});
+
+export const { useGetJobsQuery, useGetJobUrlsQuery, useCreateJobMutation, useCancelJobMutation } = jobsApi;
